6acd3ec76332ddfbac2ec71a85aa8278013672cf
salah856 <salah_elhossiny856@yahoo.com>
2021-10-25 21:44:00 +0200

    refactor

--- src/views/profile.js
+++ src/views/profile.js
@@ -1,8 +1,9 @@
 import React from "react";
-
 import { useAuth0 } from "@auth0/auth0-react";
 
+
 const Profile = () => {
+
   const { user } = useAuth0();
   const { name, picture, email } = user;
 
